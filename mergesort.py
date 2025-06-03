62dff048656b2457e2183ee0ee8d8ff43c94a37e
joshuakurth <gh@ich.rip>
2025-06-03 14:07:24 +0200

    refactored mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,45 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
 import matplotlib.pyplot as plt
+
+
+def merge_sort(sort_list):
+    if (len(sort_list) <= 1):
+        return
+    mid = len(sort_list) // 2
+    left = sort_list[:mid]
+    right = sort_list[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    i_l = 0
+    i_r = 0
+    i_iter = 0
+
+    while i_l < len(left) and i_r < len(right):
+        if left[i_l] <= right[i_r]:
+            sort_list[i_iter] = left[i_l]
+            i_l += 1
+        else:
+            sort_list[i_iter] = right[i_r]
+            i_r += 1
+        i_iter += 1
+
+    while i_l < len(left):
+        sort_list[i_iter] = left[i_l]
+        i_l += 1
+        i_iter += 1
+
+    while i_r < len(right):
+        sort_list[i_iter] = right[i_r]
+        i_r += 1
+        i_iter += 1
+
 
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+merge_sort(my_list)
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
